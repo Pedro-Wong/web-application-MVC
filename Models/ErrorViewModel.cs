2dd0d8bdb78780fb8649a1d1915e3afb88be1ad1
Pedro Azevedo <phenrique.wong@gmail.com>
2025-10-21 12:47:31 -0300

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace application_web_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
